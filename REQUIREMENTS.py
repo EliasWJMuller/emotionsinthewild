690054d9d638e32e3894d7c7382e55a82d79b804
eliasmueller <95125813+EliasWJMuller@users.noreply.github.com>
2023-03-24 18:02:04 +0100

    Update REQUIREMENTS.py

--- REQUIREMENTS.py
+++ REQUIREMENTS.py
@@ -8,15 +8,18 @@ import json
 import requests
 import urllib
 import base64
+import csv
 
 from collections import Counter
 from sklearn.preprocessing import OneHotEncoder
 from future.utils import iteritems
 from face_client import multipart
 from google.cloud import vision
+from sklearn.metrics import f1_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import cohen_kappa_score, confusion_matrix
+from scipy.optimize import minimize
 
 import numpy as np
 import pandas as pd
-
-
-
+import matplotlib.pyplot as plt
